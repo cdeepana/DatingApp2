5126cafa1a3bf82a8c2edc078e41fec13fc25622
Chathura Herath <164048p@uom.lk>
2019-03-28 13:46:48 +0530

    2019/3/28reset

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
   }
 
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
   }
   getValues() {
     this.http.get('http://localhost:5000/api/values').subscribe(Response => {
@@ -24,5 +24,9 @@ export class HomeComponent implements OnInit {
     }, error => {
       console.log(error);
     });
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
   }
 }
