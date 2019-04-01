9459a7646a9e38994361122e87bd5617fa4a5de0
Chathura Herath <164048p@uom.lk>
2019-04-01 23:50:58 +0530

    authguardnewcomponentrouting

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,17 +11,19 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
+      // console.log('Logged in successfully');
       this.alertify.success('Logged in successfully');
     }, error => {
       console.log(error);
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['members']);
     });
    }
 
@@ -32,8 +35,9 @@ export class NavComponent implements OnInit {
 
    logout() {
      localStorage.removeItem('token');
-     console.log('logged out');
+    //  console.log('logged out');
      this.alertify.message('logged out');
+     this.router.navigate(['home']);
    }
 
 }
