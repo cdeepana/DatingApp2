9661283516e67f3efb859f11cce314f7b0fa095b
Chathura Herath <164048p@uom.lk>
2019-04-17 20:50:49 +0530

    Pagination_featureSTART

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -17,7 +17,7 @@ export class MemberListComponent implements OnInit {
   ngOnInit() {
     // this.loadUsers(); old method
     this.route.data.subscribe(data => {
-      this.users = data['users'];
+      this.users = data['users'].result;
     })
   }
 
